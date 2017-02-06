492e4886d7b6e9c17722790195ec4c955004f50e
davidhenley <henleydr@gmail.com>
2017-02-06 09:57:09 -0600

    Signup form

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import App from './components/app';
 
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
 
 export default (
   <Route path="/" component={App}>
     <Route path="signin" component={Signin} />
     <Route path="signout" component={Signout} />
+    <Route path="signup" component={Signup} />
   </Route>
 );
